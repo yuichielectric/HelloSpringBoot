007f7171a76d8c8885546f11e2a86141d1b0e9f2
Yuichi Tanaka <yuichielectric@github.com>
2018-10-03 03:26:32 +0900

    Modify test to fail intentionally.

--- src/test/java/org/yuichielectric/HelloSpringBoot/HelloSpringBootApplicationTests.java
+++ src/test/java/org/yuichielectric/HelloSpringBoot/HelloSpringBootApplicationTests.java
@@ -1,9 +1,12 @@
 package org.yuichielectric.HelloSpringBoot;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +14,7 @@ public class HelloSpringBootApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		fail();
 	}
 
 }
